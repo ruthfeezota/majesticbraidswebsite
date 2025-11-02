561ef6d426a5b15966541d4d14660c6faa62a912
ruthfeezota <ruthzota@gmail.com>
2025-11-02 01:58:53 -0600

    updated sales banner

--- src/components/SaleBanner.jsx
+++ src/components/SaleBanner.jsx
@@ -6,10 +6,10 @@ function SaleBanner() {
       <div className="absolute top-0 mx-auto flex w-full items-center justify-center bg-[#2C1755] py-4">
         <p className="md:mx-auto text-center text-[#E4CBAA] font-extrabold">
           <a
-            href="https://book.squareup.com/appointments/ssrk16cq70u9xm/location/L90W7ZYT50MKG/services/RE554QWF3SI22N2IH6F4FJWQ"
+            href="https://book.squareup.com/appointments/ssrk16cq70u9xm/location/L90W7ZYT50MKG/services/UFZKZIVG7Y4RFJ7N3APS7IMZ"
             className="font-extrabold hover:underline"
           >
-            🌟 Rumi K-Pop Demon Hunter Huntrix Purple Kis Braids — Only $35! Limited Time. Book Your Child's Spot Now!
+            🌟 Women Big Box Braids — Only $100! Limited Time. Book Your Spot Now!
           </a>
         </p>
       </div>
